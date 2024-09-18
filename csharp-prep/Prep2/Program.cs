1a6398d5c67acfb7c2f3cca47ab55c70e63b1e77
roger_lingard <rogerhl@byui.edu>
2024-09-18 15:10:13 -0600

    Did the prep assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What grade did you get? ");
+        String grade = Console.ReadLine();
+        int grade_int = int.Parse(grade);
+
+        String letter = "";
+        if(grade_int >= 90)
+        {
+            letter = "A";
+        }
+        else if(grade_int < 90 && grade_int>= 80)
+        {
+            letter = "B";
+        }
+        else if(grade_int < 80 && grade_int>= 70)
+        {
+            letter = "C";
+        }
+        else if(grade_int < 70 && grade_int>= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Youre Grade is: {letter} ");
+
+        if(grade_int >= 70)
+        {
+            Console.WriteLine($"\nCongrats you did it! Good job, keep passing");
+        }
+        else
+        {
+            Console.WriteLine($"\nYou can do better next time, You got it and just try again. You know half the content at least.");
+        }
     }
 }
