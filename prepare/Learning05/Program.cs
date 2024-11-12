98a1558f377a2967349ee7f93980b7515440001e
roger_lingard <rogerhl@byui.edu>
2024-11-11 18:33:18 -0700

    prep assignment done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new();
+
+        Square square = new Square("Purple", 4);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Yellow", 5, 6);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Green", 8);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes){
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"Color: {color} \nArea: {area}");
+        }
     }
 }
